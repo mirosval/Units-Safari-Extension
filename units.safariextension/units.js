a963ed3fd25b10fb1358556735ecca48ca0d38ca
Miroslav Zoricak <miroslav.zoricak@gmail.com>
2016-05-05 23:00:13 +0200

    Added support for comma-separated thousands in miles
    Also added a little test web page that demoes basic functionality

--- units.safariextension/units.js
+++ units.safariextension/units.js
@@ -1,3 +1,7 @@
+function wrap(str) {
+  return ' (' + str + ')';
+}
+
 var replacements = [{
     /* Change heights in format 6'5" to metric units */
     pattern: /(\d+)'(\d*)("|''|)/g,
@@ -5,9 +9,9 @@ var replacements = [{
         var length = Math.round(100 * (parseInt(feet, 10) * 30.48 + parseInt(inches, 10) * 2.54)) / 100;
 
         if(length > 230) {
-            return match + ' (' + Math.round(length) / 100 + "m)";
+            return match + wrap(Math.round(length) / 100 + "m");
         } else {
-            return match + ' (' + length + "cm)";
+            return match + wrap(length + "cm");
         }
     }
 },{
@@ -16,9 +20,9 @@ var replacements = [{
         var length = Math.round(100 * (parseInt(feet, 10) * 30.48 + parseInt(inches, 10) * 2.54)) / 100;
 
         if(length > 230) {
-            return match + ' (' + Math.round(length) / 100 + "m)";
+            return match + wrap(Math.round(length) / 100 + "m");
         } else {
-            return match + ' (' + length + "cm)";
+            return match + wrap(length + "cm");
         }
     }
 },{
@@ -26,7 +30,7 @@ var replacements = [{
     func: function(match, lbs, offset, str){
         var weight = Math.round(100 * (parseFloat(lbs) * 0.453592)) / 100;
 
-        return match + ' (' + weight + "kg)";
+        return match + wrap(weight + "kg");
     }
 },{
     /* Change weight from lbs to KG */
@@ -34,15 +38,16 @@ var replacements = [{
     func: function(match, lbs, offset, str){
         var weight = Math.round(100 * (parseFloat(lbs) * 0.453592)) / 100;
 
-        return match + ' (' + weight + "kg)";
+        return match + wrap(weight + "kg");
     }
 },{
     /* Miles to km */
-    pattern: /(\d+\.?\d*) ?miles?/ig,
+    pattern: /((\d+,?)+\.?\d*) ?miles?/ig,
     func: function(match, miles, offset, str){
-        var dist = Math.round(100 * (parseFloat(miles) * 1.60934)) / 100;
+        var miles = parseFloat(miles.replace(',', ''))
+        var dist = Math.round(100 * (miles * 1.60934)) / 100;
 
-        return match + ' (' + dist + "km)";
+        return match + wrap(dist + "km");
     }
 },{
     /* Miles to km */
@@ -50,7 +55,7 @@ var replacements = [{
     func: function(match, mpg, offset, str){
         var lkm = Math.round(100 * (235.214 / parseFloat(mpg))) / 100;
 
-        return match + ' (' + lkm + "L / 100km)";
+        return match + wrap(lkm + "L / 100km");
     }
 },{
     /* Miles to km/h */
@@ -58,14 +63,14 @@ var replacements = [{
     func: function(match, miles, offset, str){
         var dist = Math.round(100 * (parseFloat(miles) * 1.60934)) / 100;
 
-        return match + ' (' + dist + "km/h)";
+        return match + wrap(dist + "km/h");
     }
 }];
 
 function replacePattern(node, patterns) {
     if(node.nodeType === Node.TEXT_NODE) {
         for(var i = 0, len = patterns.length; i < len; ++i) {
-            node.nodeValue = node.nodeValue.replace(patterns[i].pattern, patterns[i].func);    
+            node.nodeValue = node.nodeValue.replace(patterns[i].pattern, patterns[i].func);
         }
     } else if(node.nodeType === Node.ELEMENT_NODE) {
         for(var i = 0, num = node.childNodes.length; i < num; ++i) {
